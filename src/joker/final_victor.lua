f970c29fc847534cc32c2d315018872f7afa95a7
sephdotmp3 <80807134+sephdotmp3@users.noreply.github.com>
2026-03-06 15:04:39 -0600

    changed final victor functionality

--- src/joker/final_victor.lua
+++ src/joker/final_victor.lua
@@ -23,8 +23,10 @@ SMODS.Joker {
     loc_txt = {
         name = "Final Victor",
         text = { -- TODO: add card formatting
-            "Retrigger all scored cards",
-            "with Spade suit"
+            "This Joker gains {X:mult,C:white}X#2#{} Mult",
+            "each time a card is scored (max #3# times),",
+            "resets after scoring",
+            "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)",
         }
     },
     atlas = "joker_final_victor",
@@ -32,9 +34,54 @@ SMODS.Joker {
         x = 0,
         y = 0,
     },
+    config = {
+        extra = {
+            xmult = 1,
+            xmult_gain = 0.2,
+            max_triggers = 15,
+            xmult_base = 1,
+        }
+    },
     discovered = true,
     rarity = 2,
     cost = 5,
     blueprint_compat = true,
-    -- TODO: write the actual calculate function
+    perishable_compat = false,
+    calculate = function (self, card, context)
+        if context.individual and context.cardarea == G.play and not context.blueprint and
+            card.ability.extra.xmult < card.ability.extra.max_triggers*card.ability.extra.xmult_gain then
+            SMODS.scale_card(card, {
+	            ref_table = card.ability.extra,
+                ref_value = "xmult",
+	            scalar_value = "xmult_gain",
+            })
+        elseif context.joker_main and context.cardarea == G.jokers then
+            return {
+                xmult = card.ability.extra.xmult,
+                colour = G.C.XMULT
+            }
+        elseif context.after and not context.blueprint then
+            SMODS.scale_card(card, {
+	            ref_table = card.ability.extra,
+                ref_value = "xmult",
+	            scalar_value = "xmult_base",
+                operation = function (ref_table, ref_value, initial, change)
+                    ref_table[ref_value] = change
+                end,
+                scaling_message = {
+	                message = localize("k_reset"),
+	                colour = G.C.RED,
+                }
+            })
+        end
+    end,
+    loc_vars = function (self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.xmult,
+                card.ability.extra.xmult_gain,
+                card.ability.extra.max_triggers,
+            }
+        }
+    end
 }
