3d8cca806dfa10c61ce55100b90e4e5ada7afe35
sephdotmp3 <80807134+sephdotmp3@users.noreply.github.com>
2026-05-26 22:40:28 -0600

    added new erudition effect

---
+++ src/blind/erudition.lua
@@ -0,0 +1,32 @@
+--[[
+ * erudition.lua
+ * This file is part of Balatro x Star Rail
+ *
+ * Copyright (C) 2026 sephdotwmv
+ *
+ * Balatro x Star Rail is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Balatro x Star Rail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Balatro x Star Rail; if not, see <https://www.gnu.org/licenses/>.
+]]
+
+SMODS.Blind {
+    key = "erudition",
+    atlas = "blind_erudition",
+    discovered = true,
+    boss = {
+        min = 3,
+    },
+    boss_colour = HEX("20b082"),
+    modify_hand = function(self, cards, poker_hands, text, mult, hand_chips)
+        return mult/#cards, hand_chips, #cards > 1
+    end
+}
